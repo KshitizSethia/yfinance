26a71baeb74887650b021e0d0b478e4efa9e8024
Kshitiz Sethia <kshitizsethia@gmail.com>
2020-06-09 21:00:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,14 +14,14 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with io.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+#with io.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+#    long_description = f.read()
 
 setup(
     name='yfinance',
     version="0.1.54",
     description='Yahoo! Finance market data downloader',
-    long_description=long_description,
+    long_description="",
     url='https://github.com/ranaroussi/yfinance',
     author='Ran Aroussi',
     author_email='ran@aroussi.com',
